91aa6dee651f048ec8df48a288a50a5c5d1b7c36
tangxiaodong2 <tangxiaodong2@126.com>
2017-09-01 15:40:35 +0800

    upade

--- tools_jquery.js
+++ tools_jquery.js
@@ -119,7 +119,7 @@ function removeCookie(name){
 //filterParams({a:"",b:null,c:"010",d:123})
 //Object {c: "010", d: 123}
 function filterParams(obj){
-    let _newPar = {};
+    var  _newPar = {};
     for (let key in obj) {
         if ((obj[key] === 0 || obj[key]) && obj[key].toString().replace(/(^\s*)|(\s*$)/g, '') !== '') {
             _newPar[key] = obj[key];
